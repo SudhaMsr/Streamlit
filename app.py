7fc656bd32330cfb2746fe872592ec3131a0c1cd
Sudha Rani Maddala <42686314+SudhaMsr@users.noreply.github.com>
2026-03-15 12:42:34 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,3 @@
 import streamlit as st
 
-st.title("Innomatics Data App")
-st.snow()
-
-btn_click = st.button("Click Me!")
-
-if btn_click == True:
-    st.subheader("You clicked me :cry:")
-    st.balloons()
+st.header("Hello Sudhaa!! ")
